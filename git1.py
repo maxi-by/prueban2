99c20e563a3276c9b405df78c98d740c494f7519
maximo <maxi.moralesg@duocuc.cl>
2026-07-07 20:40:39 -0400

    se agrego la funcion stock por tipo de transporte y busqueda por rango de costo

--- git1.py
+++ git1.py
@@ -18,3 +18,19 @@ def leer_opcion():
                 return opc
             else:
                 print("Error: El valor ingresado debe ser entre 1-6")
+productos = {
+    'L001': ['Electrónica', 'tecnología', 'aéreo', 'Asia', False, 'Chile'],
+    'L002': ['Materiales', 'construcción', 'marítimo', 'Europa', False, 'Chile'],
+    'L003': ['Químicos', 'industrial', 'terrestre', 'Local', True, 'Chile'],
+    'L004': ['Alimentos', 'perecederos', 'marítimo', 'Latam', False, 'Chile'],
+    'L005': ['Textiles', 'vestuario', 'aéreo', 'Asia', False, 'Chile'],
+    'L006': ['Combustibles', 'energía', 'terrestre', 'Local', True, 'Chile'],
+}
+inventario = {
+    'L001': [500000, 20],
+    'L002': [300000, 50],
+    'L003': [150000, 10],
+    'L004': [250000, 30],
+    'L005': [400000, 100],
+    'L006': [200000, 5],
+}
